48b0734fab322992e021256f3ebe61664e827e7e
Abdullahshaikh-code <abdullahshakh569@gmail.com>
2023-09-27 10:21:36 -0700

    Apply Cosine Similarity to retrive data

---
+++ pytorch-chatbot-master/pytorch-chatbot-master/chat.py
@@ -0,0 +1,72 @@
+import json
+import torch
+from  sklearn.feature_extraction.text  import  TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+Tfid_Vectorizer=TfidfVectorizer(tokenizer=tokenize,stop_words="english",max_df=0.90,min_df=0.10)
+
+def get_default_device():
+    if torch.cuda.is_available():
+        print("Got CUDA!")
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+device = get_default_device()
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+data = torch.load("TrainedData.pth")
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+allWords = data["allWords"]
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    Input_sentence = input("")
+    if Input_sentence == "quit":
+        break
+    sentence = tokenize(Input_sentence)
+    X = bag_of_words(sentence, allWords)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                responses=intent["responses"]
+                responses.append(Input_sentence)
+                tfidf=Tfid_Vectorizer.fit_transform(responses)                
+                vals = cosine_similarity(tfidf[-1], tfidf)
+                idx=vals.argsort()[0][-2]
+                flat=vals.flatten()
+                flat.sort()
+                req_tfidf=flat[-2]
+                if(req_tfidf<0.1 ):
+                     print("I am sorry. Unable to understand you!")
+                     responses.remove(Input_sentence)
+
+                else:
+                   print(f"{bot_name}: {responses[idx]}")
+                   responses.remove(Input_sentence)
+                
+    else:
+        print(f"{bot_name}: I am sorry. Unable to understand you!")
